3de178f3b4e31c7c5a09869c9eb4e055cdcf79d4
INIRU <iniru@kakao.com>
2024-04-12 02:31:11 +0900

    recommand loop scroll

--- src/components/ui/Scroll.tsx
+++ src/components/ui/Scroll.tsx
@@ -7,18 +7,55 @@ import style from '@/styles/Main.module.css';
 import Card from '@/components/ui/Card';
 
 import { ScrollShadow } from '@nextui-org/react';
+import { useCallback, useLayoutEffect, useRef, useState } from 'react';
 
 export default function Scroll({ data }: { data: MediaData[] }) {
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+  const contentRef = useRef<HTMLDivElement | null>(null);
+
+  const [scroll, setScroll] = useState<number>(0);
+  const [width, setWidth] = useState<number>(0);
+
+  const handleScroll = useCallback(() => {
+    if (scrollRef.current) {
+      const scroll = scrollRef.current.scrollLeft;
+      if (scroll <= 0 || scroll >= width * 2) {
+        scrollRef.current.scrollLeft = width;
+      }
+    }
+  }, [width]);
+
+  useLayoutEffect(() => {
+    if (contentRef.current && scrollRef.current) {
+      setWidth(contentRef.current.offsetWidth);
+      scrollRef.current.scrollLeft = 1;
+    }
+  });
+
   return (
     <ScrollShadow
       hideScrollBar
       orientation='horizontal'
       offset={-10}
-      className={style.mediaContainer}
+      ref={scrollRef}
+      onScroll={handleScroll}
+      className={style.scrollContainer}
     >
-      {data.map((media, i) => {
-        return <Card key={i} isLoading={false} data={media}></Card>;
-      })}
+      <div>
+        {data.map((item, i) => {
+          return <Card key={i} isLoading={false} data={item}></Card>;
+        })}
+      </div>
+      <div ref={contentRef}>
+        {data.map((item, i) => {
+          return <Card key={i} isLoading={false} data={item}></Card>;
+        })}
+      </div>
+      <div>
+        {data.map((item, i) => {
+          return <Card key={i} isLoading={false} data={item}></Card>;
+        })}
+      </div>
     </ScrollShadow>
   );
 }
