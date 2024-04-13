05b094e0ea00221d61a15c23798d92af6f4ed5a3
INIRU <iniru@kakao.com>
2024-04-13 14:46:35 +0900

    cf recommand scroll btn

--- src/components/ui/Scroll.tsx
+++ src/components/ui/Scroll.tsx
@@ -7,13 +7,15 @@ import style from '@/styles/Main.module.css';
 import Card from '@/components/ui/Card';
 
 import { ScrollShadow } from '@nextui-org/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons/faChevronLeft';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons/faChevronRight';
 import { useCallback, useLayoutEffect, useRef, useState } from 'react';
 
 export default function Scroll({ data }: { data: MediaData[] }) {
   const scrollRef = useRef<HTMLDivElement | null>(null);
   const contentRef = useRef<HTMLDivElement | null>(null);
 
-  const [scroll, setScroll] = useState<number>(0);
   const [width, setWidth] = useState<number>(0);
 
   const handleScroll = useCallback(() => {
@@ -28,34 +30,68 @@ export default function Scroll({ data }: { data: MediaData[] }) {
   useLayoutEffect(() => {
     if (contentRef.current && scrollRef.current) {
       setWidth(contentRef.current.offsetWidth);
-      scrollRef.current.scrollLeft = 1;
+      scrollRef.current.scrollLeft = width;
     }
   });
 
   return (
-    <ScrollShadow
-      hideScrollBar
-      orientation='horizontal'
-      offset={-10}
-      ref={scrollRef}
-      onScroll={handleScroll}
-      className={style.scrollContainer}
-    >
-      <div>
-        {data.map((item, i) => {
-          return <Card key={i} isLoading={false} data={item}></Card>;
-        })}
+    <div className={style.container}>
+      <div
+        className={style.arrow}
+        onClick={() => {
+          if (scrollRef.current) {
+            scrollRef.current.scrollTo({
+              left: scrollRef.current.scrollLeft - 500,
+              behavior: 'smooth',
+            });
+          }
+        }}
+      >
+        <FontAwesomeIcon
+          icon={faChevronLeft}
+          className='text-white opacity-80'
+        />
       </div>
-      <div ref={contentRef}>
-        {data.map((item, i) => {
-          return <Card key={i} isLoading={false} data={item}></Card>;
-        })}
+      <div
+        className={style.arrow}
+        onClick={() => {
+          if (scrollRef.current) {
+            scrollRef.current.scrollTo({
+              left: scrollRef.current.scrollLeft + 500,
+              behavior: 'smooth',
+            });
+          }
+        }}
+      >
+        <FontAwesomeIcon
+          icon={faChevronRight}
+          className='text-white opacity-80'
+        />
       </div>
-      <div>
-        {data.map((item, i) => {
-          return <Card key={i} isLoading={false} data={item}></Card>;
-        })}
-      </div>
-    </ScrollShadow>
+      <ScrollShadow
+        hideScrollBar
+        orientation='horizontal'
+        offset={-10}
+        ref={scrollRef}
+        onScroll={handleScroll}
+        className={style.scrollContainer}
+      >
+        <div>
+          {data.map((item, i) => {
+            return <Card key={i} isLoading={false} data={item}></Card>;
+          })}
+        </div>
+        <div ref={contentRef}>
+          {data.map((item, i) => {
+            return <Card key={i} isLoading={false} data={item}></Card>;
+          })}
+        </div>
+        <div>
+          {data.map((item, i) => {
+            return <Card key={i} isLoading={false} data={item}></Card>;
+          })}
+        </div>
+      </ScrollShadow>
+    </div>
   );
 }
